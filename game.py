f0178d3b56612ef7a27a145f017e3b35eee39404
seigward12 <alexis.nicolas@polymtl.ca>
2021-11-23 10:40:32 -0500

    Update game.py

--- game.py
+++ game.py
@@ -18,8 +18,20 @@ class Weapon:
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
+	def __init__(self, name, power, min_level) -> None:
+		self.__name = name
+		self.poer = power
+		self.min_level = min_level
 
 	UNARMED_POWER = 20
+
+	@property
+	def name(self):
+		return self.__name
+
+	@classmethod
+	def make_unarmed(cls):
+		return cls('unarmed', cls.UNARMED_POWER)
 
 
 class Character:
@@ -32,11 +44,29 @@ class Character:
 	:param defense: Le niveau de défense du personnage
 	:param level: Le niveau d'expérience du personnage
 	"""
+	def __init__(self, name : str, max_hp, attack, defence, level) -> None:
+		self.name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defence = defence
+		self.level = level
+		self.hp = max_hp
+		self.weapon = None
 
+	@property
+	def name(self):
+		return self.__name
+
+	def compute_damage(self, a, d):
+		attaque = ((a.level*2/5 + 2) * a.weapon * (a.attack/d.defence))/50 + 2
+		modifier = (random.randint(85, 100))/100 * (2 if random.randint(1,16)==16 else 1)
+		retun attaque*modifier
 
 
 def deal_damage(attacker, defender):
 	# TODO: Calculer dégâts
+	damage = attacker.compute_damage(attacker, defender)
+	
 	print(f"{attacker.name} used {attacker.weapon.name}")
 	if crit:
 		print("  Critical hit!")
